2ac73ce27e33400dc4da5e3d5b5a9ae8676b4de6
doll2gom <shkim_ok@naver.com>
2023-04-19 15:28:55 +0900

    add 127.0.0.1

--- crud/settings.py
+++ crud/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['port-0-legendary-fortnight-e9btb72blgna3gns.sel3.cloudtype.app', 'localhost']
+ALLOWED_HOSTS = ['port-0-legendary-fortnight-e9btb72blgna3gns.sel3.cloudtype.app', 'localhost', '127.0.0.1']
 
 
 # Application definition
